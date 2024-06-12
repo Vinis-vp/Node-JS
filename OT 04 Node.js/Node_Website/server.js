bc8e46517301515775fdb1c94a01c8d967007f20
Vinis-vp <viniciusviertel955@gmail.com>
2024-06-12 19:19:46 -0300

    Instalando express e criando servidor local

---
+++ OT 04 Node.js/Node_Website/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('express'));
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname+ '/node-example-website/express/index.html'));
+});
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+
+console.log('Servidor iniciado na porta' + port);
