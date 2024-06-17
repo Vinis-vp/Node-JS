502d3e427c7d7a013f0a0ba298791e736c80b06c
Vinis-vp <viniciusviertel955@gmail.com>
2024-06-17 20:53:03 -0300

    Deletando dados via API

--- OT 05 Node.js/ListaDeGames/index.js
+++ OT 05 Node.js/ListaDeGames/index.js
@@ -53,3 +53,9 @@ app.put('/novogame/:index', (req, res) =>{
 
     return res.json(games);
 });
+
+app.delete("/:index", (req, res) =>{
+    const { index } = req.params;
+    games.splice(index,1);
+    return res.json({message: "O Jogo foi deletado"});
+});
