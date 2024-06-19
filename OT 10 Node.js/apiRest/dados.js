bfb844cd9e7143ef7ed0d68fc69bd1433cf13c0e
Vinis-vp <viniciusviertel955@gmail.com>
2024-06-19 19:56:39 -0300

    Iniciando o Projeto da API Rest

---
+++ OT 10 Node.js/apiRest/dados.js
@@ -0,0 +1,138 @@
+const colecaoUf = [
+{
+    id: 1,
+    uf: "AC",
+    nomde: "Acre"
+},
+{
+    id: 2,
+    uf: "AL",
+    nomde: "Alagoas"
+},
+{
+    id: 3,
+    uf: "AM",
+    nomde: "Amazonas"
+},
+{
+    id: 4,
+    uf: "AP",
+    nomde: "Amapá"
+},
+{
+    id: 5,
+    uf: "BA",
+    nomde: "Bahia"
+},
+{
+    id: 6,
+    uf: "CE",
+    nomde: "Ceará"
+},
+{
+    id: 7,
+    uf: "DF",
+    nomde: "Distrito Federal"
+},
+{
+    id: 8,
+    uf: "ES",
+    nomde: "Espírito Santo"
+},
+{
+    id: 9,
+    uf: "GO",
+    nomde: "Goiás"
+},
+{
+    id: 10,
+    uf: "MA",
+    nomde: "Maranhão"
+},
+{
+    id: 11,
+    uf: "MT",
+    nomde: "Mato Grosso"
+},
+{
+    id: 12,
+    uf: "MS",
+    nomde: "Mato Grosso do Sul"
+},
+{
+    id: 13,
+    uf: "MG",
+    nomde: "Minas Gerais"
+},
+{
+    id: 14,
+    uf: "PA",
+    nomde: "Pará"
+},
+{
+    id: 15,
+    uf: "PB",
+    nomde: "Paraíba"
+},
+{
+    id: 16,
+    uf: "PR",
+    nomde: "Paraná"
+},
+{
+    id: 17,
+    uf: "PE",
+    nomde: "Pernambuco"
+},
+{
+    id: 18,
+    uf: "PI",
+    nomde: "Piauí"
+},
+{
+    id: 19,
+    uf: "RJ",
+    nomde: "Rio de Janeiro"
+},
+{
+    id: 20,
+    uf: "RN",
+    nomde: "rio grande do Norte"
+},
+{
+    id: 21,
+    uf: "RS",
+    nomde: "Rio Grande do Sul"
+},
+{
+    id: 22,
+    uf: "RO",
+    nomde: "Rondônia"
+},
+{
+    id: 23,
+    uf: "RR",
+    nomde: "Roraima"
+},
+{
+    id: 24,
+    uf: "SC",
+    nomde: "Santa Catarina"
+},
+{
+    id: 25,
+    uf: "SP",
+    nomde: "São Paulo"
+},
+{
+    id: 26,
+    uf: "SE",
+    nomde: "Sergipe"
+},
+{
+    id: 27,
+    uf: "TO",
+    nomde: "Tocantins"
+}]
+
+export default colecaoUf;
